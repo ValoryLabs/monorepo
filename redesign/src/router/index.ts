8d58447f673371619ede9a0d53fe63b3c1bff457
Misha Gusev <majikx.prod0@gmail.com>
2025-03-29 04:48:20 +0500

    refactor: Rename view files and update import paths for consistency

--- redesign/src/router/index.ts
+++ redesign/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import OverlayView from '@/views/OverlayView.vue'
-import NotFoundView from '@/views/NotFoundView.vue'
+import HomeView from '@/pages/HomeView.vue'
+import OverlayView from '@/pages/OverlayView.vue'
+import NotFoundView from '@/pages/NotFoundView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
