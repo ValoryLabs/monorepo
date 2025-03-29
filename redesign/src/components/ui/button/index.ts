e3957b7d7726d3ea347f6b2bbc7c96e739a700f5
Misha Gusev <majikx.prod0@gmail.com>
2025-03-30 03:13:35 +0500

    feat: enhance button variants with improved shadow effects and transitions

--- redesign/src/components/ui/button/index.ts
+++ redesign/src/components/ui/button/index.ts
@@ -4,16 +4,18 @@ import { cva } from 'class-variance-authority'
 export { default as Button } from './Button.vue'
 
 export const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0',
+  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0',
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground shadow hover:bg-primary/90',
+        default:
+          'bg-primary text-primary-foreground shadow hover:bg-primary/90 shadow-[0_2px_0_0_#999999]',
         destructive: 'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline:
-          'border border-white/10 bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground',
+          'border border-white/10 bg-transparent shadow-transparent hover:shadow-[0_2px_0_0_#ffffff1a] hover:bg-accent hover:text-accent-foreground',
         secondary: 'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
-        ghost: 'hover:bg-accent hover:text-accent-foreground',
+        ghost:
+          'hover:bg-accent hover:text-accent-foreground shadow-transparent hover:shadow-[0_2px_0_0_hsl(var(--accent)/0.6)]',
         link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
