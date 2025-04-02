558f1ea7bc242f94126140d8c8f7c400219f4ee9
Misha Gusev <majikx.prod0@gmail.com>
2025-04-03 00:09:51 +0500

    chore: update Toaster component styles for improved visibility

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@ useSeoMeta({
   <Toaster
     position="top-right"
     :toastOptions="{
-      class: 'bg-black/80 text-[#f2f2f2] flex gap-3 backdrop-blur-sm border border-white/10',
+      class: 'bg-black/10 text-[#f2f2f2] flex gap-3 backdrop-blur-md border border-white/10',
     }"
   />
   <RouterView />
