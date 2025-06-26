aa9c5b113718248d957d569963496fc580b77e9b
Misha Gusev <majikx.prod0@gmail.com>
2025-06-27 03:39:21 +0500

    refactor: simplify LeftSidebar layout and improve styling for better usability

--- frontend/src/components/configuration-page/sidebar/left-sidebar/LeftSidebar.vue
+++ frontend/src/components/configuration-page/sidebar/left-sidebar/LeftSidebar.vue
@@ -14,16 +14,12 @@ const { fullscreen } = storeToRefs(userStore)
     v-if="!fullscreen"
     class="border flex-shrink-0 flex justify-between w-61 flex-col transition-all bg-background"
   >
-    <div class="p-2 border-b border-white/10">
+    <div class="pl-5 pt-2">
       <div
         @click="$router.push('/')"
-        class="relative flex cursor-pointer items-center justify-center gap-2 py-3"
+        class="relative flex cursor-pointer items-center justify-baseline gap-2 py-3"
       >
         <Valory class="size-6" />
-        <span
-          class="font-valory mt-px inline-block bg-linear-to-b from-[#f2f2f2] to-[#dddddd] bg-clip-text text-base leading-none text-transparent"
-          >VALORY</span
-        >
       </div>
     </div>
     <Navigation />
