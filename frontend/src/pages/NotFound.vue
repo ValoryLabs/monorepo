c15872484a94aa8a1023cb3383c65c73444f0f9b
Misha Gusev <majikx.prod0@gmail.com>
2025-09-08 04:00:34 +0500

    feat: update Valory color, button styles, russian locale text, NotFound and NotSupported pages

--- frontend/src/pages/NotFound.vue
+++ frontend/src/pages/NotFound.vue
@@ -18,20 +18,15 @@ useHead({
     <div class="m-auto flex items-center justify-center">
       <div class="container flex flex-col content-center items-center justify-center gap-3">
         <div class="relative flex flex-row items-center gap-2">
-          <Valory :size="26" />
+          <Valory :size="38" />
           <Valory :size="96" class="absolute left-0 blur-[120px]" />
-          <span
-            class="font-valory inline-block bg-linear-to-b from-[#f2f2f2] to-[#dddddd] bg-clip-text text-lg leading-none text-transparent"
-          >
-            VALORY
-          </span>
         </div>
         <div class="flex flex-col items-center justify-center gap-4 py-3">
           <span class="text-valory text-lg font-bold uppercase">
             {{ t('components.404.notfound') }}
           </span>
           <span class="text-center whitespace-pre-wrap">{{ t('components.404.description') }}</span>
-          <Button @click="router.push({ name: 'home' })" class="mt-2">
+          <Button variant="secondary" @click="router.push({ name: 'home' })" class="mt-2">
             {{ t('components.404.home') }}
           </Button>
         </div>
