b68a29b50c6a29d8ec7b70e56959628bc49b6bd8
Misha Gusev <majikx.prod0@gmail.com>
2025-09-09 00:04:12 +0500

    chore: Update Get Started section text and background colors

--- frontend/src/components/features/home-page/GetStarted.vue
+++ frontend/src/components/features/home-page/GetStarted.vue
@@ -13,7 +13,7 @@ const { t } = useI18n()
       <div
         class="bg-white/2 container relative z-10 flex flex-col items-center gap-8 overflow-hidden rounded-2xl border border-white/20 px-12 py-24 backdrop-blur-[8px]"
       >
-        <span class="text-center text-4xl font-bold">{{ t('getStarted.title') }}</span>
+        <span class="text-center text-4xl max-w-xl font-bold">{{ t('getStarted.title') }}</span>
         <span class="whitespace-pre-line max-w-3xl w-full text-center text-xl font-normal">
           {{ t('getStarted.subtitle') }}
         </span>
@@ -42,13 +42,13 @@ const { t } = useI18n()
 
 <style scoped>
 .bg-gradient {
-  background-color: rgb(122, 175, 255);
+  background-color: rgb(255, 82, 82);
   background-image:
-    radial-gradient(at 0% 0%, rgb(122, 175, 255) 0px, transparent 50%),
-    radial-gradient(at 68% 47%, rgb(98, 80, 255) 0px, transparent 50%),
-    radial-gradient(at 10% 67%, rgb(54, 255, 174) 0px, transparent 50%),
-    radial-gradient(at 62% 19%, rgb(153, 98, 255) 0px, transparent 50%),
-    radial-gradient(at 70% 29%, rgb(36, 255, 167) 0px, transparent 50%),
-    radial-gradient(at 67% 5%, rgb(255, 128, 113) 0px, transparent 50%);
+    radial-gradient(at 0% 0%, rgb(255, 82, 82) 0px, transparent 50%),
+    radial-gradient(at 68% 47%, rgb(220, 38, 127) 0px, transparent 50%),
+    radial-gradient(at 10% 67%, rgb(255, 107, 107) 0px, transparent 50%),
+    radial-gradient(at 62% 19%, rgb(185, 28, 28) 0px, transparent 50%),
+    radial-gradient(at 70% 29%, rgb(248, 113, 113) 0px, transparent 50%),
+    radial-gradient(at 67% 5%, rgb(239, 68, 68) 0px, transparent 50%);
 }
 </style>
