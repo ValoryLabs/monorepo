92f9b14148c09bb1e42689c01ece27bf9e88203d
Misha Gusev <majikx.prod0@gmail.com>
2025-09-14 04:54:03 +0500

    feat(codegen): add codegen for henrik api

--- apps/frontend/src/api/codegen-api.ts
+++ apps/frontend/src/api/codegen-api.ts
@@ -1,12 +1,12 @@
 import { fileURLToPath } from 'node:url'
 import { generateApi } from 'swagger-typescript-api'
 
-async function generateSwagger() {
+async function generateBackendSwagger() {
   let tryCount = 0
   while (tryCount < 10) {
     try {
       await generateApi({
-        fileName: 'openapi.ts',
+        fileName: 'backend-openapi.ts',
         url: 'https://api.valory.localhost/openapi.json',
         output: fileURLToPath(new URL(`./`, import.meta.url)),
         generateClient: true,
@@ -17,7 +17,7 @@ async function generateSwagger() {
         patch: true,
         generateResponses: true,
       })
-      console.log('✅ API types generated successfully!')
+      console.log('✅ Backend API types generated successfully!')
       break
     } catch (error: any) {
       console.log(`❌ Attempt ${tryCount + 1} failed:`, error.message)
@@ -27,4 +27,31 @@ async function generateSwagger() {
   }
 }
 
-await generateSwagger()
+async function generateHenrikDevSwagger() {
+  let tryCount = 0
+  while (tryCount < 10) {
+    try {
+      await generateApi({
+        fileName: 'henrikdev-openapi.ts',
+        url: 'https://beta.api.henrikdev.xyz/openapi.json',
+        output: fileURLToPath(new URL(`./`, import.meta.url)),
+        generateClient: true,
+        httpClientType: 'fetch',
+        singleHttpClient: true,
+        extractEnums: true,
+        defaultResponseAsSuccess: true,
+        patch: true,
+        generateResponses: true,
+      })
+      console.log('✅ HenrikDev API types generated successfully!')
+      break
+    } catch (error: any) {
+      console.log(`❌ Attempt ${tryCount + 1} failed:`, error.message)
+      tryCount++
+      await new Promise((resolve) => setTimeout(resolve, 1000))
+    }
+  }
+}
+
+await generateBackendSwagger()
+await generateHenrikDevSwagger()
