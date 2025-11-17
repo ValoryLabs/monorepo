80843c7b36df54f880af3ecbf40327bf963d0a62
Misha Gusev <majikx.prod0@gmail.com>
2025-11-17 23:43:56 +0500

    feat(landing): init

---
+++ apps/landing/astro.config.mjs
@@ -0,0 +1,20 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import vue from "@astrojs/vue";
+import { fileURLToPath, URL } from "node:url";
+
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  integrations: [vue()],
+
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  },
+});
